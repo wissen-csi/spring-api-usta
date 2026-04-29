ac08d13dfcbcb10b664dcf411a58cc181e29351d
Jorge <wissencsi@gmail.com>
2026-04-28 23:16:07 -0500

    add structure of project

---
+++ MedicineRotationManager/src/main/java/com/usta/edu/co/MedicineRotationManager/utils/UUIDGenerator.java
@@ -0,0 +1,18 @@
+package com.usta.edu.co.MedicineRotationManager.utils;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class UUIDGenerator {
+    @Autowired
+    private static PasswordEncoder passwordEncoder;
+    
+    public static String newId(){
+        return UUID.randomUUID().toString();
+    }
+    public static String encryptUUID(){
+        return passwordEncoder.encode(UUID.randomUUID().toString());
+    }
+}
