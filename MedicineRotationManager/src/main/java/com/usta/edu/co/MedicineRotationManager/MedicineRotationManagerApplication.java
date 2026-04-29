1976081e9d30e424dfc0e294750fb60bb1ecb483
Jorge <wissencsi@gmail.com>
2026-04-28 22:08:38 -0500

    hello people, we started with project spring

---
+++ MedicineRotationManager/src/main/java/com/usta/edu/co/MedicineRotationManager/MedicineRotationManagerApplication.java
@@ -0,0 +1,13 @@
+package com.usta.edu.co.MedicineRotationManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicineRotationManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicineRotationManagerApplication.class, args);
+	}
+
+}
---
+++ MedicineRotationManager/src/test/java/com/usta/edu/co/MedicineRotationManager/MedicineRotationManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usta.edu.co.MedicineRotationManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicineRotationManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
