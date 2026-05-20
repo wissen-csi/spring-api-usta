dbf4adff41ea7690e33db2de77c880079578b459
Jorge <wissencsi@gmail.com>
2026-05-19 22:43:18 -0500

    examples of restControllers

---
+++ MedicineRotationManager/src/main/java/com/usta/edu/co/MedicineRotationManager/dto/responseDTOS/AdminResponseDTO.java
@@ -0,0 +1,36 @@
+package com.usta.edu.co.MedicineRotationManager.dto.responseDTOS;
+
+import java.time.LocalDate;
+
+import com.usta.edu.co.MedicineRotationManager.enumerations.MaritalStatus;
+import com.usta.edu.co.MedicineRotationManager.enumerations.TypeBlood;
+
+import lombok.Builder;
+@Builder
+public record AdminResponseDTO(
+        String id,
+
+        String name,
+
+        String lastName,
+
+        String dni,
+
+        String email,
+
+        String phoneNumber,
+
+        MaritalStatus maritalStatus,
+
+        TypeBlood typeBlood,
+
+        double weight,
+
+        double imc,
+
+        LocalDate hiringDate,
+
+        LocalDate endDate
+) {
+
+}
