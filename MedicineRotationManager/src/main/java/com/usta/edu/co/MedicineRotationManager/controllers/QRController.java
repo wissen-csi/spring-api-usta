3147177dfc95ec36001b3d7e4d14f81246ada0fc
Jorge <wissencsi@gmail.com>
2026-05-22 20:43:44 -0500

    add endpoints creationm qr

---
+++ MedicineRotationManager/src/main/java/com/usta/edu/co/MedicineRotationManager/controllers/QRController.java
@@ -0,0 +1,47 @@
+package com.usta.edu.co.MedicineRotationManager.controllers;
+
+import java.io.IOException;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.zxing.WriterException;
+import com.usta.edu.co.MedicineRotationManager.models.EntryPractice;
+import com.usta.edu.co.MedicineRotationManager.services.QRService;
+import com.usta.edu.co.MedicineRotationManager.services.ServiceEntryPractice;
+
+
+@RestController
+@RequestMapping("/qr")
+public class QRController {
+
+    private final QRService qrService;
+    private final ServiceEntryPractice serviceEntryPractice;
+
+
+    public QRController(QRService qrService, ServiceEntryPractice serviceEntryPractice) {
+        this.qrService = qrService;
+        this.serviceEntryPractice = serviceEntryPractice;
+    }
+
+
+    @GetMapping("/generate/{id}")
+    public ResponseEntity<byte[]> generateQR(
+            @RequestParam String id)
+            throws WriterException, IOException {
+                EntryPractice entryPractice = serviceEntryPractice.findById(id);
+
+        byte[] qrImage = qrService.generateQR(entryPractice.getQrCode(), 300, 300);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=qr.png")
+                .contentType(MediaType.IMAGE_PNG)
+                .body(qrImage);
+    }
+}
+
