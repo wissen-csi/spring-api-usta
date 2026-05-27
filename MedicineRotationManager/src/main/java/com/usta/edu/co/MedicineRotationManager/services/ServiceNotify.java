3cfe2db8bd696e3cf76bcea7e04cf208a0be900d
JonathanLMJ <jonathan.malaver@uptc.edu.co>
2026-05-27 03:25:31 -0500

    Final commit in this branch

--- MedicineRotationManager/src/main/java/com/usta/edu/co/MedicineRotationManager/services/ServiceNotify.java
+++ MedicineRotationManager/src/main/java/com/usta/edu/co/MedicineRotationManager/services/ServiceNotify.java
@@ -15,30 +15,30 @@ import jakarta.mail.MessagingException;
 
 @Service
 public class ServiceNotify {
-private MailManager mailManager;
-private ServiceStudent serviceStudent;
-private ServicePerson servicePerson;
+    private MailManager mailManager;
+    private ServiceStudent serviceStudent;
+    private ServicePerson servicePerson;
 
-public ServiceNotify(MailManager mailManager, ServiceStudent serviceStudent, ServicePerson servicePerson) {
-    this.mailManager = mailManager;
-    this.serviceStudent = serviceStudent;
-    this.servicePerson = servicePerson;
-}
+    public ServiceNotify(MailManager mailManager, ServiceStudent serviceStudent, ServicePerson servicePerson) {
+        this.mailManager = mailManager;
+        this.serviceStudent = serviceStudent;
+        this.servicePerson = servicePerson;
+    }
 
-public void sendCloseToArl() throws Exception{
-    Context context = new Context();
-    List<MessageStudentDTO> list= serviceStudent
-    .findCloseToExpireARL()
-    .stream()
-    .map(x->new MessageStudentDTO(x.getName()+" "+x.getLastName(), x.getDni(), x.getArlEndDate(), !LocalDate.now().isBefore(x.getArlEndDate()))).toList();
-    context.setVariable("students", list);
-    List<String> emailsAdmin = servicePerson.findEmailsByRole(AppRole.ADMIN);
-    emailsAdmin.forEach(x -> {
-        try {
-            mailManager.sendNotifyArl(x, TemplateEnum.ARL, context, "close to expire ARL");
-        } catch (MessagingException e) {
-            e.printStackTrace();
-        }
-    });
-}
+    public void sendCloseToArl() throws Exception {
+        Context context = new Context();
+        List<MessageStudentDTO> list = serviceStudent
+                .findCloseToExpireARL()
+                .stream()
+                .map(x -> new MessageStudentDTO(x.getName() + " " + x.getLastName(), x.getDni(), x.getArlEndDate(), !LocalDate.now().isBefore(x.getArlEndDate()))).toList();
+        context.setVariable("students", list);
+        List<String> emailsAdmin = servicePerson.findEmailsByRole(AppRole.ADMIN);
+        emailsAdmin.forEach(x -> {
+            try {
+                mailManager.sendNotifyArl(x, TemplateEnum.ARL, context, "close to expire ARL");
+            } catch (MessagingException e) {
+                e.printStackTrace();
+            }
+        });
+    }
 }
